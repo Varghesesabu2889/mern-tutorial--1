4a900e68951e34410fe92a709c3728e9c674bd21
varghesesabu2889 <varghesesabu16@gmail.com>
2024-05-23 13:10:44 +0530

    new commit

--- server.js
+++ server.js
@@ -1,1 +1,54 @@
-console.log("hello");
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 8000
+const app = express()
+
+
+//using simple 
+// app.get('/api/goals',(req,res)=>{
+//     res.send(`It's a Goal`)
+// })
+
+//using json 
+// app.get('/api/goals',(req,res)=>{
+//     res.json({
+//         message:`It's a Goal`
+//     })
+// })
+//using json and with status code
+app.get('/api/goals',(req,res)=>{
+    res.status(201).json({
+        message:`It's a Goal`
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
